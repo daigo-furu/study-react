7c325c833472541af3601eebe56a54bbe6d6e429
daigo-furu <naomi_furuuchi@yahoo.co.jp>
2024-10-30 01:12:50 +0900

    vol3：Headline部分作成

---
+++ src/components/Headline.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import styles from "@/app/page.module.css"
+
+export function Headline() {
+  return (
+    <>
+      <h1>Index Page</h1>
+      <Image
+        className={styles.logo}
+        src="/next.svg"
+        alt="Next.js logo"
+        width={180}
+        height={38}
+        priority
+      />
+      <ol>
+        <li>
+          Get started by editing <code>src/app/page.tsx</code>.
+        </li>
+        <li>Save and see your changes instantly.</li>
+      </ol>
+    </>
+  );
+}
