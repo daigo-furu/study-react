bc86d9048d4e414385b5136801dbfc5021d053f7
daigo-furu <naomi_furuuchi@yahoo.co.jp>
2024-12-14 23:28:59 +0900

    vol15：カスタムフックを使ってまとめた

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,64 +1,31 @@
 "use client";
 import Link from "next/link"
 import styles from "./Header.module.css"
-import { useCallback, useState,useEffect } from "react";
+import { useEffect } from "react";
+import { useCounter } from "@/hooks/useCounter";
+import { useInputArray } from "@/hooks/useInputArray";
+
 
 // 外部だと引数の渡しがめんどくさいがコンポーネントが再レンダリングされてもメソッドは再生成されない
 // <a>に使う場合はイベントの型はReact.MouseEvent<HTMLAnchorElement>で、React.MouseEventだけでも一応大丈夫
-const handleClick1 = (e: React.MouseEvent<HTMLAnchorElement>, foo: number) =>{
-  console.log(e.target)
-  e.preventDefault()
-  alert(foo);
-}
-
-
+// const handleClick1 = (e: React.MouseEvent<HTMLAnchorElement>, foo: number) =>{
+//   console.log(e.target)
+//   e.preventDefault()
+//   alert(foo);
+// }
 
 export function Header() {
-  const [count, setCount] = useState(1)
-  const [text, setText] = useState("")
-  const [isShow, setIsShow] = useState(true)
-  //
-  const [array, setArray] = useState<string[]>([])
+  const {count, isShow, handleClick1, handleClick2, handleClick3, handleDisplay} = useCounter()
+  const {text, array, handelChange, handleAdd} = useInputArray()
 
   // 内部だとコンポーネントが再レンダリングされるとメソッドは再生成される
   // <button>の場合はReact.MouseEvent<HTMLButtonElement>でReact.MouseEventだけでも一応大丈夫
-  const handleClick2 = (e: React.MouseEvent<HTMLButtonElement>) => {
-    console.log(e.target); // HTMLButtonElement として扱われます
-    setCount(count => count + 1)
-    setCount(count => count + 1)
-  };
-  // useCallbackを使うと再レンダリングされたときに再生成されない
-  const handleClick3 = useCallback((e: React.MouseEvent<HTMLButtonElement>) => {
-    console.log(e.target);
-    // alert(count);
-    console.log(count);
-    if(count < 10){
-      setCount(prevCount => prevCount + 1)
-    }
-  }, [count]);
+  // const handleClick2 = (e: React.MouseEvent<HTMLButtonElement>) => {
+  //   console.log(e.target); // HTMLButtonElement として扱われます
+  //   setCount(count => count + 1)
+  //   setCount(count => count + 1)
+  // };
 
-  const handleDisplay = useCallback(() => {
-    setIsShow((prevIsShow) =>  !prevIsShow)
-  },[])
-
-
-  const handelChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-    if(e.target.value.length > 5){
-      alert("5文字以内にしてください")
-      return
-    }
-    setText(e.target.value.trim())
-  },[])
-
-  const handleAdd = useCallback(() =>{
-    setArray((prevArray) => {
-      if (prevArray.some(item => item === text)){
-        alert("同じ要素がすでに存在します。")
-        return prevArray
-      }
-      return [...prevArray, text]
-    })
-  },[text])
 
   useEffect(() => {
     console.log(`マウント時：${count}`)
@@ -77,19 +44,27 @@ export function Header() {
           About
         </Link>
       </div>
-      <p>
-        <a
-          href="/about"
-          onClick={(e) => {handleClick1(e, count)}}>ボタン1
-        </a>
-      </p>
       <div>
-        <button onClick={handleClick2}>ボタン2（カウント）</button>
-        <button onClick={handleClick3}>ボタン3</button>
+
+
+        {isShow ? <h2>{count}</h2> : null}
+        <p>
+          <a
+            href="/about"
+            onClick={(e) => {handleClick1(e, count)}}>ボタン1
+          </a>
+        </p>
+        <button onClick={handleClick2}>ボタン2（2カウント）</button>
+
+
+        <button onClick={handleClick3}>ボタン3（10までカウント）</button>
         <button onClick={handleDisplay}
         >
           {isShow ? "非表示" : "表示"}
         </button>
+
+
+
         <input type="text" value={text}
           onChange={handelChange}
         />
@@ -102,7 +77,9 @@ export function Header() {
           })}
         </ul>
       </div>
-      {isShow ? <h2>{count}</h2> : null}
+
+
+
     </header>
   );
 }
